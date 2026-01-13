1eabf51bf440c7a0fe1ee99f4d9605baac8e18e6
MatysChampeyrol <champeyrolmatys.pro@gmail.com>
2026-01-13 17:08:54 +0100

    conf : add conf in vite.config.ts for github pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/cy-weather/',  // Nom de votre repo
+  build: {
+    outDir: '../docs',    // Build dans docs/ à la racine
+    emptyOutDir: true
+  }
 })
