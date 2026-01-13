9b6ba83fc51a14be00aa0bfa2de21f11b8890881
MatysChampeyrol <champeyrolmatys.pro@gmail.com>
2026-01-13 17:21:45 +0100

    fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,9 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/cy-weather/',  // Nom de votre repo
-  build: {
-    outDir: '../docs',    // Build dans docs/ à la racine
-    emptyOutDir: true
-  }
+  base: '/cy-weather/',
 })
